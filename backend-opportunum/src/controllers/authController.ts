e481e44f0cbc9bb165392bf9650d9de17c6f095c
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-08-29 17:13:42 -0300

    ajustes deploy

--- backend-opportunum/src/controllers/authController.ts
+++ backend-opportunum/src/controllers/authController.ts
@@ -73,9 +73,9 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // só HTTPS em produção
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 24 * 60 * 60 * 1000, // 1 dia
+      secure: true, 
+      sameSite: "none" ,
+      maxAge: 24 * 60 * 60 * 1000, 
     });
 
     res.status(200).json({

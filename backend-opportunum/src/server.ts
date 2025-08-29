f7c74a254a1024f9daa31668305315663da10afd
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-08-29 16:37:16 -0300

    ajustes deploy

--- backend-opportunum/src/server.ts
+++ backend-opportunum/src/server.ts
@@ -11,13 +11,18 @@ import taskRoutes from './routes/taskRoutes';
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",            
-  "https://opportunum.vercel.app",   
-];
-
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "https://opportunum.vercel.app"
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 }));
 app.use(express.json());

edf215e090058a0c7701a4703b65d3e7b989f2f6
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-09-08 20:44:37 -0300

    fix(auth): adicionar middlaware de proteção antes do middleware de permissões para rota de deletar tarefa

--- backend-opportunum/src/routes/taskRoutes.ts
+++ backend-opportunum/src/routes/taskRoutes.ts
@@ -6,12 +6,13 @@ import {
   deleteTask
 } from '../controllers/taskController';
 import { authorizeRoles } from '../middleware/authorizeRoles';
+import { protect } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.post('/', createTask);
 router.get('/:projId', getTasksByProject);
 router.put('/:id', updateTask);
-router.delete('/:id', authorizeRoles( "master", "admin"), deleteTask);
+router.delete('/:id', protect, authorizeRoles( "master", "admin"), deleteTask);
 
 export default router;
